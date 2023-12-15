8caa3cdb8ab4ffc715ac09d648a450e00a017549
Simon0qwer <saradiansimon@gmail.com>
2023-12-15 11:36:43 +0100

    Password migration

--- WebChat/Model/User.cs
+++ WebChat/Model/User.cs
@@ -4,7 +4,7 @@ public class User
 {
     public Guid Id { get; set; } = Guid.NewGuid();
     public string Name { get; set; } = string.Empty;
-
+    public string Password { get; set; } = string.Empty;
     public ICollection<Message> Messages { get; set; } = new List<Message>();
     public ICollection<Chat> Chats { get; set; } = new List<Chat>();
 }
